7dbfccb9245e59f418aae5b16269eb35f4424eda
leviaran <randy.arba@gmail.com>
2020-08-27 16:44:28 +0700

    init

---
+++ app/src/androidTest/java/com/esspresso/testespresso/testUi.kt
@@ -0,0 +1,90 @@
+package com.esspresso.testespresso
+
+import android.view.View
+import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.internal.matchers.TypeSafeMatcher
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ChangeUiTest {
+
+    @Rule
+    @JvmField
+    val activityScenarioRules = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun changeTestActivity() {
+//        R.id.et_username
+//        R.id.et_pass
+//        R.id.btn_action
+//        R.id.et_status
+        onView(withId(R.id.et_username)).perform(typeText("Hallo"), closeSoftKeyboard())
+            .check(matches(withText("Hallo")))
+
+        onView(withId(R.id.et_pass)).perform(typeText("12345678"), closeSoftKeyboard())
+            .check(matches(withFontSize(8)))
+
+        onView(withId(R.id.btn_action)).perform(click())
+        onView(withId(R.id.et_status)).perform(setTextViewVisibitity(true))
+        onView(withId(R.id.et_status)).check(matches(isDisplayed()))
+    }
+
+    fun withFontSize(expectedSize: Int): Matcher<View> {
+        return object :
+            BoundedMatcher<View, TextView>(TextView::class.java) {
+            override fun matchesSafely(target: TextView): Boolean {
+                return true
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("with fontSize: ")
+                description?.appendValue(expectedSize)
+            }
+        }
+    }
+
+
+    private fun setTextViewVisibitity(value: Boolean): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                view.visibility = if (value) View.VISIBLE else View.GONE
+            }
+
+            override fun getDescription(): String {
+                return "Show / Hide View"
+            }
+        }
+    }
+
+    fun isTextInLines(lines: Int): TypeSafeMatcher<View?> {
+        return object : TypeSafeMatcher<View?>() {
+            override fun matchesSafely(item: View?): Boolean {
+                return (item as TextView).lineCount == lines
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("isTextInLines")
+            }
+
+        }
+    }
+}
